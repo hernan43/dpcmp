876b7fd6be492a7ede882a0b31beab1da25d6b13
Ray Hernandez <hernan43@gmail.com>
2011-06-18 15:50:30 -0400

    Added a simple list

--- dpcompare/dpcompare.go
+++ dpcompare/dpcompare.go
@@ -15,13 +15,15 @@ import (
   "resize"
   "crypto/sha1"
   "template"
+  "time"
 )
 
 // Image is the type used to hold the image in the datastore.
 type Comparison struct {
-        Left []byte
-        Right []byte
-        Submitter string
+  Left []byte
+  Right []byte
+  Submitter string
+  Date datastore.Time
 }
 
 
@@ -32,6 +34,10 @@ func (c *Comparison) key() string {
   sha.Write(c.Right)
   sha.Write([]byte(c.Submitter))
   return fmt.Sprintf("%x", string(sha.Sum())[0:16])
+}
+
+func (c Comparison) String() string {
+  return fmt.Sprintf("%s", c.key())
 }
 
 var templates = make(map[string]*template.Template)
@@ -110,7 +116,7 @@ func makeHandler(fn func(http.ResponseWriter, *http.Request, appengine.Context, 
   }
 }
 
-func renderTemplate(w http.ResponseWriter, tmpl string, m map[string] string) {
+func renderTemplate(w http.ResponseWriter, tmpl string, m map[string] interface{}) {
   err := templates[tmpl].Execute(w, m)
   if err != nil {
     http.Error(w, err.String(), http.StatusInternalServerError)
@@ -119,7 +125,7 @@ func renderTemplate(w http.ResponseWriter, tmpl string, m map[string] string) {
 
 func init() {
   /*cache templates*/
-  for _, tmpl := range []string{"index", "show"} {
+  for _, tmpl := range []string{"index", "list", "show"} {
     templates[tmpl] = template.MustParseFile(tmpl+".html", nil)
   }
 
@@ -128,17 +134,18 @@ func init() {
   http.HandleFunc("/upload", makeHandler(upload))
   http.HandleFunc("/show", makeHandler(show))
   http.HandleFunc("/img", makeHandler(img))
+  http.HandleFunc("/list", makeHandler(list))
 }
 
 func index(w http.ResponseWriter, r *http.Request, c appengine.Context, u *user.User) {
-  templateContext := make(map[string] string)
+  templateContext := make(map[string] interface{})
   templateContext["username"] = u.Email
   // render
   renderTemplate(w, "index", templateContext)
 }
 
 func upload(w http.ResponseWriter, r *http.Request, c appengine.Context, u *user.User) {
-  templateContext := make(map[string] string)
+  templateContext := make(map[string] interface{})
   if r.Method != "POST" {
     templateContext["username"] = u.Email
     renderTemplate(w, "index", templateContext)
@@ -149,6 +156,8 @@ func upload(w http.ResponseWriter, r *http.Request, c appengine.Context, u *user
   comparison.Left = extractImageFromPost("left_picture", r)
   comparison.Right = extractImageFromPost("right_picture", r)
   comparison.Submitter = u.Email
+  comparison.Date = datastore.SecondsToTime(time.Seconds()) // now
+
 
   // Save the comparison under a unique key, a hash of the struct's data
   key := datastore.NewKey("Comparison", comparison.key(), 0, nil)
@@ -184,7 +193,8 @@ func img(w http.ResponseWriter, r *http.Request, c appengine.Context, u *user.Us
 }
 
 func show(w http.ResponseWriter, r *http.Request, c appengine.Context, u *user.User) {
-  templateContext := make(map[string] string)
+  //templateContext := make(map[string] string)
+  templateContext := make(map[string] interface{})
   templateContext["username"] = u.Email
 
   key := r.FormValue("id")
@@ -192,4 +202,18 @@ func show(w http.ResponseWriter, r *http.Request, c appengine.Context, u *user.U
   // render
   renderTemplate(w, "show", templateContext)
 }
+
+func list(w http.ResponseWriter, r *http.Request, c appengine.Context, u *user.User) {
+  templateContext := make(map[string] interface{})
+  templateContext["username"] = u.Email
+  // setup datastore access
+  q := datastore.NewQuery("Comparison").Order("-Date")
+	count, _ := q.Count(c)
+	comparisons := make([]Comparison, 0, count)
+	q.GetAll(c, &comparisons)
+	templateContext["comparisons"] = comparisons
+  // render
+  renderTemplate(w, "list", templateContext)
+}
+
 
